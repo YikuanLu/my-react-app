6bbbcc8b739cffdf0a51db757a86cfd2f310e6a9
卢逸宽 <lyk9406@foxmail.com>
2020-03-07 15:38:08 +0800

    feat:添加一些配置

--- src/components/public/showName.tsx
+++ src/components/public/showName.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Button } from 'antd'
+import logo from '@/assets/img/logo-sm.png'
 
 export interface Props {
   myName: string;
@@ -10,6 +11,7 @@ export interface Props {
 const ShowName: React.FC<Props> = ({ myName, age = 3 }: Props) => {
   return (
     <div className="showName">
+      <img style={{ width: '30px' }} src={logo} alt="" />
       <div>名字：{myName}</div>
       <div>岁数：{age}</div>
       <Button type="primary">Button</Button>
